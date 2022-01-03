fe35f0e8c28a8b35b9c23ee6cca5f7d6229080ab
abdelrhmanhsh <abdelrhmanhsh@gmail.com>
2022-01-03 16:59:47 +0200

    navigation

--- src/tic/tac/toe/TicTacToe.java
+++ src/tic/tac/toe/TicTacToe.java
@@ -6,27 +6,26 @@
 package tic.tac.toe;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+/**
+ *
+ * @author Abdelrhman
+ */
 public class TicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
+        Parent root = new LoginBase(stage);
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
